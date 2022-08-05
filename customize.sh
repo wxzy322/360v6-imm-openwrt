89b7d80ddb729baed372c085efab407a9447b1c3
chainsx <pzhrhlx@outlook.com>
2022-08-05 13:49:30 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-#=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: eSirPlayground
-# Youtube Channel: https://goo.gl/fvkdwm 
-#=================================================
+
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.8.1/g' openwrt/package/base-files/files/bin/config_generate
 
@@ -13,4 +8,4 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-se
 
 #3. 360v6 wireless config file.
 rm openwrt/package/firmware/ath11k-firmware/files/board-2.bin.IPQ6018
-cp bin/board-2.bin.360v6 openwrt/package/firmware/ath11k-firmware/files/board-2.bin.IPQ6018
+cp bin/board-2.bin openwrt/package/firmware/ath11k-firmware/files/board-2.bin.IPQ6018
