09ab99578476ac216c7c6e9477bab29989f661ed
chainsx <pzhrhlx@outlook.com>
2022-08-05 13:48:55 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -11,3 +11,6 @@ sed -i 's/192.168.1.1/192.168.8.1/g' openwrt/package/base-files/files/bin/config
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
+#3. 360v6 wireless config file.
+rm openwrt/package/firmware/ath11k-firmware/files/board-2.bin.IPQ6018
+cp bin/board-2.bin.360v6 openwrt/package/firmware/ath11k-firmware/files/board-2.bin.IPQ6018
