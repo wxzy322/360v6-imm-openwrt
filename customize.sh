1dd182f503f175df349116935de7bcfcc3ad1352
chainsx <pzhrhlx@outlook.com>
2022-10-27 23:47:07 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-#1. Modify default IP
 sed -i 's/192.168.1.1/192.168.8.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/chainsx-bee/g' openwrt/package/base-files/files/bin/config_generate
 
-#2. Clear the login password
-sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i 's/16384/65535/g' openwrt/package/kernel/linux/files/sysctl-nf-conntrack.conf
+sed -i 's/OpenWrt/chainsx-bee/g' openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-#3. 360v6 wireless config file.
+sed -i 's/US/CN/g' openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
 rm openwrt/package/firmware/ath11k-firmware/files/board-2.bin.IPQ6018
 cp bin/board-2.bin openwrt/package/firmware/ath11k-firmware/files/board-2.bin.IPQ6018
