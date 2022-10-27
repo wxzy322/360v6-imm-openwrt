dde1e9b9a3f1098a44365e3e89660d6fc44b3c7b
chainsx <pzhrhlx@outlook.com>
2022-10-28 02:32:57 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,45 +6,6 @@ sed -i 's/192.168.1.1/192.168.8.1/g' openwrt/package/base-files/files/bin/config
 # 修改主机名
 sed -i 's/OpenWrt/chainsx-bee/g' openwrt/package/base-files/files/bin/config_generate
 
-# 修改时区
-sed -i 's/UTC/Asia\/Shanghai/g' openwrt/package/base-files/files/bin/config_generate
-
-# 修改luci配置
-echo "
-config core 'main'
-        option resourcebase '/luci-static/resources'
-        option lang 'zh_cn'
-        option mediaurlbase '/luci-static/argon'
-
-config extern 'flash_keep'
-        option uci '/etc/config/'
-        option dropbear '/etc/dropbear/'
-        option openvpn '/etc/openvpn/'
-        option passwd '/etc/passwd'
-        option opkg '/etc/opkg.conf'
-        option firewall '/etc/firewall.user'
-        option uploads '/lib/uci/upload/'
-
-config internal 'languages'
-        option zh_cn '简体中文 (Simplified Chinese)'
-
-config internal 'sauth'
-        option sessionpath '/tmp/luci-sessions'
-        option sessiontime '3600'
-
-config internal 'ccache'
-        option enable '1'
-
-config internal 'themes'
-        option Argon '/luci-static/argon'
-        option Bootstrap '/luci-static/bootstrap'
-
-config internal 'diag'
-        option dns 'openwrt.org'
-        option ping 'openwrt.org'
-        option route 'openwrt.org'
-" > openwrt/extra/luci/modules/luci-base/root/etc/config/luci
-
 # 修改连接数
 sed -i 's/16384/65535/g' openwrt/package/kernel/linux/files/sysctl-nf-conntrack.conf
 
