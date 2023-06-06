990683151f4bcca0612c813ccab37e6112afb7f5
chainsx <pzhrhlx@outlook.com>
2023-06-07 00:35:46 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 修改 lan IP
-sed -i 's/192.168.1.1/192.168.8.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.3.1/g' openwrt/package/base-files/files/bin/config_generate
 
 # 修改主机名
-sed -i 's/OpenWrt/chainsx-bee/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/router-v6/g' openwrt/package/base-files/files/bin/config_generate
 
 # 修改连接数
 sed -i 's/16384/65535/g' openwrt/package/kernel/linux/files/sysctl-nf-conntrack.conf
