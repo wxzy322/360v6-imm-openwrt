7932e9d3edd423980c340b1e67b98f7db76c7958
wxzy322 <90735892+wxzy322@users.noreply.github.com>
2024-06-10 14:56:14 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -15,4 +15,4 @@ sed -i 's/16384/65535/g' openwrt/package/kernel/linux/files/sysctl-nf-conntrack.
 # 修改 wifi 地区
 #sed -i 's/US/CN/g' openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '1i src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
